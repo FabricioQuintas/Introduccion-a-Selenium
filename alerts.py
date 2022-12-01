b4e008c5c5f407ebe1e09a1219dbdcbd396d91f7
Fabricio Quintas <fabri97n@gmail.com>
2022-11-30 22:23:56 -0300

    Class 12 - Manage alerts and pop-up

---
+++ alerts.py
@@ -0,0 +1,38 @@
+import unittest
+from selenium import webdriver
+from api_data_mock import ApiDataMock # our user values
+
+
+class RegisterNewUser(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'C:\chromedriver.exe')
+        driver = self.driver
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        driver.get('http://demo.onestepcheckout.com/')
+
+    def test_compare_products_removal_alert(self):
+        driver = self.driver
+        search_field = driver.find_element_by_class_name('q') # Find the search field
+        search_field.clear() # Clear search_field
+
+        search_field.send_keys('tee') # Search 'tee' on search field
+        search_field.submit() # Submit package
+
+        driver.find_element_by_class_name('link-compare').click() # Add an element to compare
+        driver.find_element_by_link_text('Clear All').click() # Clear comparation list
+
+        alert = driver.swith_to.alert # Change to the web alert
+        alert_text = alert.text # Take his text
+
+        self.assertEqual('Are you sure you would like to remove all products from your comparison?', alert_text)
+        alert.accept # Accept that alert
+
+    def tearDown(self):
+        self.driver.implicitly_wait(3)
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
