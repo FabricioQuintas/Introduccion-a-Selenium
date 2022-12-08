a919a507023298f8505edc18b821e80125f6e236
Fabricio Quintas <fabri97n@gmail.com>
2022-12-07 23:39:58 -0300

    Challenge solved, creating a list with dicts for the data table

--- tables.py
+++ tables.py
@@ -1,6 +1,5 @@
 import unittest
 from selenium import webdriver
-from time import sleep
 
 
 class Tables(unittest.TestCase):
@@ -15,17 +14,26 @@ class Tables(unittest.TestCase):
     def test_sort_tables(self):
         driver = self.driver
 
-        table_data = [[] for i in range(5)]
+        # Get the len of the head data ( Table 1 ), getting a list of elements from his css selector ( -1 for the edit/delete panel )
+        columns_size = len(driver.find_elements_by_css_selector('#table1 > thead > tr > th')) - 1
+        # Get the len of data size ( in rows ) from the table 1 ( Without header )
+        row_size = len(driver.find_elements_by_css_selector('#table1 > tbody > tr'))
+
+        table_data = [[] for i in range(row_size)] # Create a list with empty lists inside (the quantity of rows of the body)
         print(table_data)
 
-        for i in range(5):
-            header = driver.find_element_by_xpath(f'//*[@id="table1"]/thead/tr/th[{i + 1}]/span')
-            table_data[i].append(header.text)
+        # Lets save all data
+        for i in range(row_size): # Iter through each row of the data
 
-            for j in range(4):
-                row_data = driver.find_element_by_xpath(f'//*[@id="table1"]/tbody/tr[{j + 1}]/td[{j + 1}]')
-                table_data[i].append(row_data.text)
-        
+            # Now iter through each column of the data
+            for j in range(columns_size):
+                # Take each head data text of the table
+                header_data = driver.find_element_by_xpath(f'//*[@id="table1"]/thead/tr/th[{j + 1}]/span').text # j will iter column by column
+                # Take each body data text
+                row_data = driver.find_element_by_xpath(f'//*[@id="table1"]/tbody/tr[{i + 1}]/td[{j + 1}]').text # i iter row by row
+                # Append to the table data, as a dict
+                table_data[i].append({header_data : row_data})
+
         print(table_data)
 
     def tearDown(self):   
